e3a5c96b693a4d447221b3ff546b3cd0c18e1636
TirthakBM <tirthakbm123@gmail.com>
2022-07-24 04:43:32 +0530

    first commit

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  const ResponsiveLayout(
+      {Key? key,
+      required this.mobileScreenLayout,
+      required this.webScreenLayout})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 900) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
---
+++ lib/widgets/web_profile_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/colors.dart';
+
+class WebProfileBar extends StatelessWidget {
+  const WebProfileBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.077,
+      width: MediaQuery.of(context).size.width * 0.25,
+      padding: const EdgeInsets.all(10.0),
+      decoration: const BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            color: dividerColor,
+          ),
+        ),
+        color: webAppBarColor,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
+            ),
+            radius: 20,
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.comment, color: Colors.grey),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
